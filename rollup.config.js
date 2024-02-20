9eac308bc3008918eb5d7a8bd0c086f5f2f54bbe
chenzexi6 <chenzexi6@jd.com>
2024-02-20 15:57:22 +0800

    fix: 增加代码打包压缩

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import terser from '@rollup/plugin-terser'
+
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/bundle.js', 
-    format: 'es', 
+    file: 'dist/bundle.js',
+    format: 'es',
     name: 'CacheManager',
-  }
-};
+  },
+  plugins: [terser()],
+}
